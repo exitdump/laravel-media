6a723438c3e1c72712990e8d42aad963d8548b1b
exitdump <jahidhasan100101@gmail.com>
2025-04-29 04:16:26 +0600

    register config files for default settings and added publishable vendor

--- src/LaravelMediaServiceProvider.php
+++ src/LaravelMediaServiceProvider.php
@@ -11,16 +11,17 @@ class LaravelMediaServiceProvider extends ServiceProvider
         $this->app->singleton('laravel-media', function ($app) {
             return new LaravelMedia();
         });
-        // $this->mergeConfigFrom(
-        //     __DIR__.'/../config/laravel-media.php', 'laravel-media'
-        // );
+
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-media.php', 'laravel-media'
+        );
     }
 
     public function boot()
     {
-        // $this->publishes([
-        //     __DIR__.'/../config/laravel-media.php' => config_path('laravel-media.php'),
-        // ], 'config');
+        $this->publishes([
+            __DIR__.'/../config/laravel-media.php' => config_path('laravel-media.php'),
+        ], 'config');
 
         // if ($this->app->runningInConsole()) {
         //     $this->commands([
