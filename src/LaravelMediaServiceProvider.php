868527098dcf77e8264f3e61678b3df7bbe3b2f6
exitdump <jahidhasan100101@gmail.com>
2025-04-29 03:09:58 +0600

    init

---
+++ src/LaravelMediaServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Exitdump\LaravelMedia;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMediaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // $this->mergeConfigFrom(
+        //     __DIR__.'/../config/laravel-media.php', 'laravel-media'
+        // );
+    }
+
+    public function boot()
+    {
+        // $this->publishes([
+        //     __DIR__.'/../config/laravel-media.php' => config_path('laravel-media.php'),
+        // ], 'config');
+
+        // if ($this->app->runningInConsole()) {
+        //     $this->commands([
+        //         Console\InstallCommand::class,
+        //     ]);
+        // }
+        // dd('working');
+    }
+}
