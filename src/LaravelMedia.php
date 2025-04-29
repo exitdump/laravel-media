70061e8c4a6f70bc140751e6a58a820f1b57df7e
exitdump <jahidhasan100101@gmail.com>
2025-04-30 04:50:45 +0600

    feat: implement upload method in LaravelMedia class with MediaHandler integration

--- src/LaravelMedia.php
+++ src/LaravelMedia.php
@@ -2,10 +2,33 @@
 
 namespace Exitdump\LaravelMedia;
 
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+ use Exitdump\LaravelMedia\MediaHandler;
+
 class LaravelMedia
 {
-    public function handle()
-    {
-        return 'its working';
+    protected $handler;
+
+    public function init(MediaHandler $handler){
+        $this->handler = $handler;
     }
+
+
+    public function upload(UploadedFile $file)
+    {
+        $user = User::find(1);
+        return $this->handler->upload($file, $user);
+    }
+    
+    // public function associateWith($model)
+    // {
+    //     $this->model = $model;
+    //     return $this;
+    // }
+    
+    // public function toCollection($collection)
+    // {
+    //     // Your implementation
+    // }
 }
