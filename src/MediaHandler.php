91d5b985da575631ef524657d837669d3583c781
exitdump <jahidhasan100101@gmail.com>
2025-05-17 03:57:29 +0600

    feat: enhance upload method in MediaHandler to support original file name preservation and improved path management

--- src/MediaHandler.php
+++ src/MediaHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Exitdump\LaravelMedia;
 
 use Exitdump\LaravelMedia\Models\Media;
@@ -8,21 +9,42 @@ use Illuminate\Support\Facades\Storage;
 class MediaHandler
 {
     public function upload(UploadedFile $file, $model = null, $collection = 'default')
-    { 
-        $disk = config('laravel-media.default_disk');
-        
+    {
+        $disk = config('laravel-media.default_disk', 'public');
+        $pathPrefix = config('laravel-media.path_prefix', 'media');
+        $preserveOriginalName = config('laravel-media.preserve_original_name', false);
+
+        $fileName = $preserveOriginalName
+            ? $this->generateOriginalFileName($file)
+            : $file->hashName();
+
+        $storagePath = $pathPrefix . '/' . date('Y/m/d');
+        $fullPath = $file->storeAs($storagePath, $fileName, $disk);
+
         $media = new Media([
-            'name' => pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME),
-            'file_name' => $file->store('', ['disk' => $disk]),
-            'mime_type' => $file->getClientMimeType(),
-            'disk' => $disk,
-            'size' => $file->getSize(),
+            'name'       => pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME),
+            'file_name'  => $fullPath,
+            'mime_type'  => $file->getClientMimeType(),
+            'disk'       => $disk,
+            'size'       => $file->getSize(),
+            'collection' => $collection,
         ]);
 
         if ($model) {
             $model->media()->save($media);
+        } else {
+            $media->save();
         }
-        
+
         return $media;
+    }
+
+    protected function generateOriginalFileName(UploadedFile $file)
+    {
+        $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $extension = $file->getClientOriginalExtension();
+        $timestamp = now()->timestamp;
+
+        return $originalName . '-' . $timestamp . '.' . $extension;
     }
 }
