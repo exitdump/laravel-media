548804ed6d1286617ee8c97f8437650a6705f630
exitdump <jahidhasan100101@gmail.com>
2025-05-17 05:00:10 +0600

    feat: add LaravelMediaPicker component for media selection in views

---
+++ src/View/Components/LaravelMediaPicker.php
@@ -0,0 +1,12 @@
+<?php
+namespace Exitdump\LaravelMedia\View\Components;
+
+use Illuminate\View\Component;
+
+class LaravelMediaPicker extends Component
+{
+    public function render()
+    {
+        return view('laravel-media::components.media-picker');
+    }
+}
